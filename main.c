ae09de2f9bc5458dee644812dad5f12e3be8a8dc
Haritha Kusal <harithakusal@gmail.com>
2021-11-23 14:54:10 +0530

    *

--- main.c
+++ main.c
@@ -4,42 +4,56 @@
 */
 
 #include <stdio.h>
+#include <time.h>
+
+void collatzConj();
+
+int n, i;
+FILE *fptr;
 
 int main()
 {
-    int n;
-    FILE *fptr;
-
+    clock_t begin = clock();
     fptr = fopen("./out1.txt","w");
 
-    for (int i = 1; i < 100; i++)
+    for (i = 1; i <= 100; i++)
     {
-        n = i;
-        printf("Number %d - ", n);
-        fprintf(fptr,"Number %d - ",n);
-        
-        while(n > 1)
+        collatzConj();
+    }
+    
+    fprintf(fptr,"\n");
+
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    
+    fprintf(fptr,"Time Elapsed: %lfs", time_spent);
+    fclose(fptr);
+}
+
+// Collatz Conjecture
+void collatzConj()
+{
+    n = i;
+    printf("%d- ", n);
+    fprintf(fptr,"%d- ",n);   
+
+    while(n > 1)
+    {
+        if(n % 2 == 0)
         {
-            if(n % 2 == 0)
-            {
-                n = n / 2;
-                printf("%d ", n);
+            n = n / 2;
+            printf("%d ", n);
+            fprintf(fptr,"%d ",n);
 
-                fprintf(fptr,"%d",n);
-
-            }
-            else
-            {
-                n = n * 3 + 1;
-                printf("%d ", n);
-
-                fprintf(fptr,"%d",n);
-            }
         }
-
-        printf("\n");
-        fprintf(fptr,"\n");
+        else
+        {
+            n = n * 3 + 1;
+            printf("%d ", n);
+            fprintf(fptr,"%d ",n);
+        }
     }
 
-    fclose(fptr);
+    printf("\n");
+    fprintf(fptr,"\n");
 }
